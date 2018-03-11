c6f59aa011f538dd61bdc18504f02affa27aba9d
murch1 <murch@outlook.com.au>
2018-03-11 22:34:49 +1000

    Update index.js

--- index.js
+++ index.js
@@ -1,41 +1,104 @@
+// This node is designed to work with local Node-RED flow
+// Refer is included flow for more information
+
+var http = require('http');
 var Service, Characteristic;
+var Pilot, Button;
 
 module.exports = function(homebridge) {
   Service = homebridge.hap.Service;
   Characteristic = homebridge.hap.Characteristic;
-  homebridge.registerAccessory("homebridge-fakebulb", "FakeBulb", FakeBulbAccessory);
+  homebridge.registerAccessory("homebridge-http-light", "http-light", LightAccessory);
 }
 
-function FakeBulbAccessory(log, config) {
+function LightAccessory(log, config) {
   this.log = log;
   this.name = config["name"];
-  this.bulbName = config["bulb_name"] || this.name; // fallback to "name" if you didn't specify an exact "bulb_name"
-  this.binaryState = 0; // bulb state, default is OFF
-  this.log("Starting a fake bulb device with name '" + this.bulbName + "'...");
+  this.lightName = config["light_name"] || this.name; // fallback to "name" if you didn't specify an exact "light_name"
+  this.binaryState = 0;                               // light state, default is OFF
+  this.switchAddr = config["switchAddr"] || 16394;    // Modbus address on remote PLC to switch light on/off (momentary/ push button)
+  this.statusAddr = config["statusAddr"] || 16994;    // Modbus address on remote PLC to read status of light
+  
 //  this.search();
 }
 
-FakeBulbAccessory.prototype.getPowerOn = function(callback) {
-  var powerOn = this.binaryState > 0;
-  this.log("Power state for the '%s' is %s", this.bulbName, this.binaryState);
-  callback(null, powerOn);
+function getData(pilot,callback) {
+  var param = 'raddr=' + pilot + '&rtype=c';
+//console.log(data);
+  var req = null;
+  var options = {
+        hostname: '127.0.0.1',          // NodeRED local HTTP server
+        port: '1880',                   // NodeRED port
+        path: '/modbus&' + param,       // HTTP access point + parameters
+        method: 'GET',
+  };
+  req = http.request(options, function (res) {
+    res.setEncoding('utf8');
+    res.on('data', function (chunk) {
+        console.log('Light: ' + chunk);
+	callback(chunk);
+    });
+  });
+  req.on('error', function(e) {
+    console.log(pilot + ' problem with request: ' + e.message);
+    callback(-1);
+  });
+  req.end();
 }
 
-FakeBulbAccessory.prototype.setPowerOn = function(powerOn, callback) {
-  this.binaryState = powerOn ? 1 : 0; // wemo langauge
-  this.log("Set power state on the '%s' to %s", this.bulbName, this.binaryState);
-  callback(null);
+function getSetData(pilot,button,callback) {
+  var param = 'write=true&waddr=' + button + '&wtype=c&wmode=mom&raddr=' + pilot + '&rtype=c';
+  var req = null;
+  var options = {
+        hostname: '127.0.0.1',          // NodeRED local HTTP server
+        port: '1880',                   // NodeRED port
+        path: '/modbus&' + param,       // HTTP access point + parameters
+        method: 'GET',
+  };
+  req = http.request(options, function (res) {
+    res.setEncoding('utf8');
+    res.on('data', function (chunk) {
+        callback(chunk);
+    });
+  });
+  req.on('error', function(e) {
+    callback(-1);
+  });
+  req.end();
 }
 
-FakeBulbAccessory.prototype.getServices = function() {
-    var lightbulbService = new Service.Lightbulb(this.name);
+
+LightAccessory.prototype.getPowerOn = function(callback) {  
+  var Pilot = this.statusAddr;
+  getData(Pilot, function(returnValue) {
+	  LightAccessory.prototype.binaryState = returnValue;
+    callback(null,returnValue);
+  });
+}
+
+LightAccessory.prototype.setPowerOn = function(powerCmd, callback) {
+  var Pilot = this.statusAddr;
+  var Button = this.switchAddr;
+  getData(Pilot, function(returnValue) {
+	if(returnValue != powerCmd) {
+	  getSetData(Pilot,Button,function(returnValue2) {
+		  LightAccessory.prototype.binaryState = returnValue2;
+				});
+			callback(null);	
+	} else {
+		callback(null);
+	}
+  });
+
+}
+
+LightAccessory.prototype.getServices = function() {
+    var lightService = new Service.Light(this.name);
     
-    lightbulbService
+    lightService
       .getCharacteristic(Characteristic.On)
       .on('get', this.getPowerOn.bind(this))
       .on('set', this.setPowerOn.bind(this));
     
-    return [lightbulbService];
+    return [lightService];
 }
-
-
